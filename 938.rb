94b25e6a88d54a33c355da70db0533925931e30d
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-03-16 12:48:58 +0000

    solve Range Sum of BST

---
+++ 938.rb
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val = 0, left = nil, right = nil)
+        @val = val
+        @left = left
+        @right = right
+    end
+end
+# @param {TreeNode} root
+#Range Sum of BST
+
+# @param {Integer} low
+# @param {Integer} high
+# @return {Integer}
+
+
+
+def range_sum_bst(root, low, high)
+    result = 0
+    unless root.nil?
+        puts result
+
+        result += root.val if root.val >= low && root.val <= high
+        result += range_sum_bst(root.right, low, high) unless root.right.nil?
+        result += range_sum_bst(root.left, low, high) unless root.left.nil?
+    end
+    return result
+end
